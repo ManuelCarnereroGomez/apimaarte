a0af7298e8082bdf8ca538ab6b55c348a54d5a85
ManuelCarnereroGomez <mcarnerero3240@esdmadrid.es>
2022-05-31 20:36:31 +0200

    second commit

--- Assets/hoja.js
+++ Assets/hoja.js
@@ -1,4 +1,6 @@
-const url = 'http://cab.inta-csic.es/rems/rems_weather.xml';
+// api de Lourdes https://mars-weather-rems.netlify.app/rems.json
+
+ const url = 'http://cab.inta-csic.es/rems/rems_weather.xml';
 
 let tiempo = {
     tempMinima: '',
@@ -35,6 +37,55 @@ function ajax() {
     }).fail(function () {
         alert("URL No Valida");
     })
-}
+} 
+
+
+
+
+// window.onload = () => {
+//     const url = 'http://cab.inta-csic.es/rems/rems_weather.xml';
+
+//     let tiempo = {
+//         tempMinima: '',
+//         tempMaxima: '',
+//         presion: '',
+//         tempSueloMin: '',
+//         tempSueloMax: '',
+//         estado: ''
+//     };
+
+//     fetch('http://cab.inta-csic.es/rems/rems_weather.xml')
+//         .then(res => res.json())
+//         .then(res => {
+//             console.log(res);
+//             tiempo.tempMinima = res.magnitudes.min_temp;
+//             tiempo.tempMaxima = res.magnitudes.max_temp;
+//             tiempo.presion = res.magnitudes.pressure;
+//             tiempo.tempSueloMin = res.magnitudes.min_gts_temp;
+//             tiempo.tempSueloMax = res.magnitudes.max_gts_temp;
+//             tiempo.estado = res.magnitudes.atmo_opacity;
+//             // tiempo.tempMinima = (respuesta.find("magnitudes").find('min_temp')[0].textContent);
+//             // tiempo.tempMaxima = (respuesta.find("magnitudes").find('max_temp')[0].textContent);
+//             // tiempo.presion = (respuesta.find("magnitudes").find('pressure')[0].textContent);
+//             // tiempo.tempSueloMin = (respuesta.find("magnitudes").find('min_gts_temp')[0].textContent);
+//             // tiempo.tempSueloMax = (respuesta.find("magnitudes").find('max_gts_temp')[0].textContent);
+//             // tiempo.estado = (respuesta.find("magnitudes").find('atmo_opacity')[0].textContent);
+//             document.getElementById('#tempMinima').innerHTML(tiempo.tempMinima)
+//             document.getElementById('#tempMaxima').innerHTML(tiempo.tempMaxima)
+//             document.getElementById('#presion').innerHTML(tiempo.presion)
+//             document.getElementById('#tempSueloMin').innerHTML(tiempo.tempSueloMin)
+//             document.getElementById('#tempSueloMax').innerHTML(tiempo.tempSueloMax)
+//             document.getElementById('#estado').innerHTML(tiempo.estado)
+//         })
+// }
+
+
+
 
 /*console.log(($(respuesta).find("magnitudes").find('min_temp')))*/
+
+
+
+
+
+
