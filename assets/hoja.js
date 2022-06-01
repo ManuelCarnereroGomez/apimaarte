0a68fb3cf5865addf4d0733726392f29d0c82505
ManuelCarnereroGomez <mcarnerero3240@esdmadrid.es>
2022-06-01 20:54:54 +0200

    add temps

--- assets/hoja.js
+++ assets/hoja.js
@@ -23,21 +23,35 @@ function ajax() {
         dataType: "json" // formato en el que se llama.
     }).done(function (respuesta) { // donde entra la llamada si es correcta, respuesta, lo que trae (xml)
         console.log(respuesta); 
-        console.log(respuesta.weather_report.magnitudes[0].min_temp[0])
+        console.log(respuesta.weather_report.magnitudes[0].min_temp[0]);
+        console.log(respuesta.weather_report.magnitudes[0].max_temp[0]);
+        console.log(respuesta.weather_report.magnitudes[0].pressure[0]);
+        console.log(respuesta.weather_report.magnitudes[0].min_gts_temp[0]);
+        console.log(respuesta.weather_report.magnitudes[0].max_gts_temp[0]);
+        console.log(respuesta.weather_report.magnitudes[0].atmo_opacity[0]);
+        
         /*tiempo.tempMinima = ($(respuesta).find("magnitudes").find('min_temp')[0].textContent);
         tiempo.tempMaxima = ($(respuesta).find("magnitudes").find('max_temp')[0].textContent);
-        tiempo.presion = ($(respuesta).find("magnitudes").find('pressure')[0].textContent);
+        tiempo.presion = ($(respuesta).find("magnitudes").find('presion')[0].textContent);
         tiempo.tempSueloMin = ($(respuesta).find("magnitudes").find('min_gts_temp')[0].textContent);
         tiempo.tempSueloMax = ($(respuesta).find("magnitudes").find('max_gts_temp')[0].textContent);
         tiempo.estado = ($(respuesta).find("magnitudes").find('atmo_opacity')[0].textContent);*/
         tiempo.tempMinima = respuesta.weather_report.magnitudes[0].min_temp[0];
         tiempo.tempMaxima = respuesta.weather_report.magnitudes[0].max_temp[0];
+        tiempo.presion = respuesta.weather_report.magnitudes[0].presion;
+        tiempo.tempSueloMin = respuesta.weather_report.magnitudes[0].min_gts_temp[0];
+        tiempo.tempSueloMax = respuesta.weather_report.magnitudes[0].max_gts_temp[0];
+        tiempo.estado  = respuesta.weather_report.magnitudes[0].estado;
+        // tiempo.pressure = respuesta.weather_report.magnitudes[0].presion[0];
 
+    
         
         //console.log(tiempo);
         $('#tempMinima').html(tiempo.tempMinima);
         $('#tempMaxima').html(tiempo.tempMaxima);
+
         $('#presion').html(tiempo.presion);
+
         $('#tempSueloMin').html(tiempo.tempSueloMin);
         $('#tempSueloMax').html(tiempo.tempSueloMax);
         $('#estado').html(tiempo.estado);
