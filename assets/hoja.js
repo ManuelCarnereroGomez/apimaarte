4e0531f569ded7a9c35e5a3e9bc75ca659d927a1
ManuelCarnereroGomez <mcarnerero3240@esdmadrid.es>
2022-06-01 20:05:50 +0200

    json

--- assets/hoja.js
+++ assets/hoja.js
@@ -1,6 +1,8 @@
 // api de Lourdes https://mars-weather-rems.netlify.app/rems.json
 
- const url = 'https://cab.inta-csic.es/rems/rems_weather.xml';
+ //const url = 'https://cab.inta-csic.es/rems/rems_weather.xml';
+
+ const url = "https://mars-weather-rems.netlify.app/rems.json";
 
 let tiempo = {
     tempMinima: '',
@@ -18,16 +20,21 @@ $(function () {
 function ajax() {
     $.ajax({
         url: url, // hacer la llamada, sacar los datos.
-        dataType: "xml" // formato en el que se llama.
+        dataType: "json" // formato en el que se llama.
     }).done(function (respuesta) { // donde entra la llamada si es correcta, respuesta, lo que trae (xml)
         console.log(respuesta); 
-        tiempo.tempMinima = ($(respuesta).find("magnitudes").find('min_temp')[0].textContent);
+        console.log(respuesta.weather_report.magnitudes[0].min_temp[0])
+        /*tiempo.tempMinima = ($(respuesta).find("magnitudes").find('min_temp')[0].textContent);
         tiempo.tempMaxima = ($(respuesta).find("magnitudes").find('max_temp')[0].textContent);
         tiempo.presion = ($(respuesta).find("magnitudes").find('pressure')[0].textContent);
         tiempo.tempSueloMin = ($(respuesta).find("magnitudes").find('min_gts_temp')[0].textContent);
         tiempo.tempSueloMax = ($(respuesta).find("magnitudes").find('max_gts_temp')[0].textContent);
-        tiempo.estado = ($(respuesta).find("magnitudes").find('atmo_opacity')[0].textContent);
-        console.log(tiempo);
+        tiempo.estado = ($(respuesta).find("magnitudes").find('atmo_opacity')[0].textContent);*/
+        tiempo.tempMinima = respuesta.weather_report.magnitudes[0].min_temp[0];
+        tiempo.tempMaxima = respuesta.weather_report.magnitudes[0].max_temp[0];
+
+        
+        //console.log(tiempo);
         $('#tempMinima').html(tiempo.tempMinima);
         $('#tempMaxima').html(tiempo.tempMaxima);
         $('#presion').html(tiempo.presion);
